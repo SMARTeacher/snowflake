2d1cbe8ae4a55240a96b1a7db226f6e06338f397
Derek Gallant <derek.gallant@prodigygame.com>
2020-01-09 15:45:18 -0500

    Fix more spacing issues

--- target-roles/backend.js
+++ target-roles/backend.js
@@ -14,7 +14,6 @@ export default [
     {
       displayName: 'Tech Lead, Backend Development',
       milestones: [3, 2, 3, 3, 2, 2, 2, 2, 1, 2, 2, 2, 1, 1, 1, 2],
-                  
     },
     {
       displayName: 'Engineering Manager',
@@ -28,5 +27,5 @@ export default [
       displayName: 'Director of Engineering',
       milestones: [3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 3, 3, 4, 3],
     },
-  ];
+];
   
